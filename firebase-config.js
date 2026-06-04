622f8ff4b69efb0cd368c98a0445e4dd497e51f5
jdvasquezf1975 <jdvasquezf@gmail.com>
2026-06-03 23:05:12 -0600

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -12,7 +12,7 @@
 const FIREBASE_CONFIG = {
   apiKey:            "AIzaSyCj1X7V0MzpMgRRsuQzzzPOS4zCa041Hmc",
   authDomain:        "elfaro-jornadas.firebaseapp.com",
-  databaseURL:       "https://elfaro-jornadas-default-rtdb.firebaseio.com"
+  databaseURL:       "https://elfaro-jornadas-default-rtdb.firebaseio.com",
   projectId:         "elfaro-jornadas",
   storageBucket:     "elfaro-jornadas.firebasestorage.app",
   messagingSenderId: "655060043608",
